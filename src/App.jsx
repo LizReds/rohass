3bbb22f2e89899dfe10e0ab06fe3ffccb520279e
LizReds <arwen.reds@gmail.com>
2022-03-02 17:06:16 -0500

    Creacion de componentes CartWidget y ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
 
 
-export const App = () => {
+const App = () => {
     return (
     <div>
         <Navbar />
-        
+        <ItemListContainer greeting="¡Hola! Bienvenido a RoHass, aguacates de alta calidad."/>
     </div>
-
     )
 }
 
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ItemListContainer = ({greeting}) => {
+    return (
+    <h1 className='m-5 flex justify-center italic text-green-700 font-medium text-lg'>
+        {greeting}
+    </h1>
+    )
+};
+
+export default ItemListContainer
